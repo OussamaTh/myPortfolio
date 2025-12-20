6e31b911f6550685dedce377247bcce3a112a478
OussamaTh <touhamioussama2206@gmail.com>
2025-12-20 12:44:26 +0000

    hh

--- src/components/parallaxEffects/parallax1.jsx
+++ src/components/parallaxEffects/parallax1.jsx
@@ -1,19 +1,16 @@
 import { motion, useScroll, useTransform } from "framer-motion";
 import { useEffect, useRef, useState } from "react";
 
-
-
 export default function Parallax1() {
   const [width, setWidth] = useState(window.innerWidth);
 
   useEffect(() => {
     const handleResize = () => setWidth(window.innerWidth);
-
     window.addEventListener("resize", handleResize);
     return () => window.removeEventListener("resize", handleResize);
   }, []);
 
-
+  const isSmallScreen = width < 640;
 
   const ref = useRef(null);
 
@@ -22,23 +19,26 @@ export default function Parallax1() {
     offset: ["start center", "end start"]
   });
 
-  const textOpacity = useTransform(scrollYProgress, [0, 0.7], [0, 1])
-  /* const textScale = useTransform(scrollYProgress,[0,0.8],[0,1]) */
-  const textLetterSpacing = useTransform(scrollYProgress, [0, 0.8], ["128px", "-20px"])
-  const smTextLetterSpacing = useTransform(scrollYProgress, [0, 0.8], ["20px", "-2px"])
+  const textOpacity = useTransform(scrollYProgress, [0, 0.8], [0, 1]);
+  const animatedLetterSpacing = useTransform(scrollYProgress,[0, 0.8],["128px", "-20px"]);
+  const animatedTextScale = useTransform(scrollYProgress,[0,0.7],[0.2,1.3]);
+
+  const textLetterSpacing = isSmallScreen ? "0px": animatedLetterSpacing;
+  const textScale = !isSmallScreen  ? 1 : animatedTextScale
 
   return (
-    <div className="min-h-[100vh] bg-black flex items-center justify-center relative ">
+    <div className="min-h-[100vh] bg-black flex items-center justify-center relative">
       <motion.div
         ref={ref}
-        className="flex h-[250vh] font-[600] flex items-center justify-center "
+        className="flex h-[250vh] font-[600] items-center justify-center"
       >
         <motion.h1
           style={{
             opacity: textOpacity,
-            letterSpacing: width < 500 ? textLetterSpacing : smTextLetterSpacing
+            letterSpacing: textLetterSpacing,
+            scale: textScale
           }}
-          className="sticky top-[40%] text-[white] text-[5rem] whitespace-nowrap text-center max-sm:text-[1.2rem] "
+          className="sticky top-[40%] text-white text-[5rem] whitespace-nowrap text-center max-sm:text-[2.5rem]"
         >
           My Experience
         </motion.h1>
