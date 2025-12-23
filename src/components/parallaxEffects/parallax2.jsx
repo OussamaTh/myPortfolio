b47de3214e116f6e8f0b1b1e3cbf05a6ba882cab
OussamaTh <touhamioussama2206@gmail.com>
2025-12-23 22:30:08 +0000

    d

--- src/components/parallaxEffects/parallax2.jsx
+++ src/components/parallaxEffects/parallax2.jsx
@@ -12,30 +12,30 @@ function Parallax2() {
     const sq2 = useTransform(scrollYProgress, [0, 0.6], [500, 0]);
     const sq3 = useTransform(scrollYProgress, [0, 0.6], [800, 0]);
 
-    let titleStyle = "jersey text-[3rem] max-md:text-[1.5rem] max-sm:text-[1.2rem] font-[400] flex flex-col justify-center items-center text-black";
-    let squareStyle = "w-[300px] h-[300px] max-lg:w-[250px] max-lg:h-[250px] max-md:w-[150px] max-md:h-[150px] max-sm:w-[100px] max-sm:h-[100px] bg-white rounded-2xl flex items-center justify-center";
+    let titleStyle = "jersey text-[3rem] max-md:text-[2rem]  font-[400] flex flex-col justify-center items-center text-black";
+    let squareStyle = "w-[300px] h-[300px] max-lg:w-[250px] max-lg:h-[250px] max-md:w-[200px] max-md:h-[200px] bg-white rounded-2xl flex items-center justify-center";
 
     return (
         <>
             <motion.section className="min-h-[350vh] relative bg-black">
                 <motion.div ref={ref} className=" min-h-[300vh] relative flex items-center justify-around gap-[1rem]">
-                    <motion.div className="flex items-center justify-around w-full sticky top-[20%]">
+                    <motion.div className="flex max-sm:flex-col gap-[1rem] items-center justify-around w-full sticky top-[20%] max-sm:top-[10%]">
                         <motion.div style={{ y: sq1 }} className={squareStyle}>
                             <h1 className={titleStyle} >
                                 <div className="">#CODING</div>
-                                <img src="./myPortfolio/icons/laptop.png" className="w-[80px] max-md:w-[50px] max-sm:w-[30px]" alt="" />
+                                <img src="./myPortfolio/icons/laptop.png" className="w-[80px] max-md:w-[50px] max-sm:w-[40px]" alt="" />
                             </h1>
                         </motion.div>
                         <motion.div style={{ y: sq2 }} className={squareStyle}>
                             <h1 className={titleStyle} >
                                 <div className="">#COFFEE</div>
-                                <img src="./myPortfolio/icons/coffee.png" className="w-[80px] max-md:w-[50px] max-sm:w-[30px]" alt="" />
+                                <img src="./myPortfolio/icons/coffee.png" className="w-[80px] max-md:w-[50px] max-sm:w-[40px]" alt="" />
                             </h1>
                         </motion.div>
                         <motion.div style={{ y: sq3 }} className={squareStyle}>
                             <h1 className={titleStyle} >
                                 <div className="">#DEBUGGING</div>
-                                <img src="./myPortfolio/icons/bug.png" className="w-[80px] max-md:w-[50px] max-sm:w-[30px]" alt="" />
+                                <img src="./myPortfolio/icons/bug.png" className="w-[80px] max-md:w-[50px] max-sm:w-[40px]" alt="" />
                             </h1>
                         </motion.div>
                     </motion.div>
